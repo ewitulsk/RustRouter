faaa4f185c135d0c333d21a50f0ace00f3bdff43
Evan W <ewitulsk@calpoly.edu>
2023-05-19 08:22:58 -0700

    generalized token in/out demo, OH! btw, output amount works for pancake pairs... forgot to mention that in the last commit

--- router/src/main.rs
+++ router/src/main.rs
@@ -42,16 +42,12 @@ fn main() {
     }
 
 
-
-
     let mut registry_map: HashMap<PairNames, RegistryTypes> = all_registrys(network.clone());
     let mut registrys: Vec<&mut RegistryTypes> = Vec::new();
 
     for (_, registry) in registry_map.iter_mut() {
         registrys.push(registry);
     }
-
-
 
     let mut pairs: Vec<PairTypes> = Vec::new();
     let metadata: HashMap<String, PancakeMetadata>;
@@ -64,6 +60,8 @@ fn main() {
             metadata = get_pancake_metadata(network);
         }
     }
+
+    write_pair_descriptors(&pairs);
 
     let mut refreshed_pancake_pairs:Vec<PancakePair> = Vec::new();
 
@@ -84,15 +82,22 @@ fn main() {
         }
     }
 
-    let token = String::from("0xa1ea1833696326fbef7d135a4fa318e4cf3f3365329b145ee37969628c8ee7bb::LeagueOfShogunsToken::LeagueOfShoguns>");
+
+
+
+    let token_in = String::from("0x1::aptos_coin::AptosCoin");
+    let in_decimal = 8;
+    let token_out: String = String::from("0x7fd500c11216f0fe3095d0c4b8aa4d64a4e2e04f83758462f2b127255643615::thl_coin::THL");
+    let out_decimal = 8;
+    let input_amount = decimal_to_u64(1.0, in_decimal);
+
+
+
 
     let mut count = 0;
     for pancake_pair in &refreshed_pancake_pairs {
-        let token_x = &pancake_pair.base.token_arr[0];
-        let token_y = &pancake_pair.base.token_arr[1];
-        if token_x == &String::from("0x1::aptos_coin::AptosCoin") && token_y == &String::from("0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T") {
-            println!("IDX: {}, X: {}, Y: {}", count, token_x ,token_y);
-            // println!("BalX: {}, BalY: {}, K: {}", (&pancake_pair.metadata.reserves.as_ref()).unwrap()[0], (&pancake_pair.metadata.reserves.as_ref()).unwrap()[1], (&pancake_pair.metadata.last_k).unwrap());
+        
+        if pancake_pair.base.token_arr.contains(&token_in) && pancake_pair.base.token_arr.contains(&token_out) {
             println!("ResX: {}, ResY: {}", (&pancake_pair.metadata.reserves.as_ref()).unwrap()[0], (&pancake_pair.metadata.reserves.as_ref()).unwrap()[1]);
 
             break;
@@ -100,21 +105,13 @@ fn main() {
         count += 1;
     }   
 
-    let mut APT_USDC_pair = refreshed_pancake_pairs.get_mut(count).unwrap();
-    let token_x = &APT_USDC_pair.base.token_arr[0];
-    let token_y = &APT_USDC_pair.base.token_arr[1];
-    // println!("BalX: {}, BalY: {}, K: {}", (&APT_USDC_pair.metadata.reserves.as_ref()).unwrap()[0], (&APT_USDC_pair.metadata.reserves.as_ref()).unwrap()[1], (&APT_USDC_pair.metadata.last_k).unwrap());
-    println!("ResX: {}, ResY: {}", (&APT_USDC_pair.metadata.reserves.as_ref()).unwrap()[0], (&APT_USDC_pair.metadata.reserves.as_ref()).unwrap()[1]);
+    let mut selected_pair = refreshed_pancake_pairs.get_mut(count).unwrap();
+    let token_x = &selected_pair.base.token_arr[0];
+    let token_y = &selected_pair.base.token_arr[1];
+    println!("ResX: {}, ResY: {}", (&selected_pair.metadata.reserves.as_ref()).unwrap()[0], (&selected_pair.metadata.reserves.as_ref()).unwrap()[1]);
 
-    //Token X: APT
-    //Token Y: whUSDC
-    let token_in = token_x;
-    let in_decimal = 8;
-    let token_out = token_y;
-    let out_decimal = 6;
-    let input_amount = decimal_to_u64(1.0, in_decimal);
 
-    let amount_out = APT_USDC_pair.output_amount(input_amount, token_in.to_string(), token_out.to_string());
+    let amount_out = selected_pair.output_amount(input_amount, token_in.to_string(), token_out.to_string());
 
     println!("In: {}, Out: {}", u64_to_decimal(input_amount, in_decimal), u64_to_decimal(amount_out, out_decimal));
    
